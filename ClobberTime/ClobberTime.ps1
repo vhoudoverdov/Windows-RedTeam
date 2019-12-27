cad14911535c66e2942ab83fb82a1528f98456a1
Vasken Houdoverdov <vhcomptech@gmail.com>
2019-12-27 14:01:05 -0800

    remove dependency on external timstomp-utils import

--- ClobberTime/ClobberTime.ps1
+++ ClobberTime/ClobberTime.ps1
@@ -13,7 +13,6 @@
     Extends the functionality of the Timestomp module located in https://github.com/vhoudoverdov/Security-Utils/.EXAMPLE
 #> 
 
-. '.\TimestompUtils\TimestompUtils.ps1'
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
@@ -21,7 +20,7 @@ Add-Type -AssemblyName System.Drawing
 
 $Form   = New-Object system.Windows.Forms.Form
 $Form.ClientSize    = '630,181'
-$Form.text  = "cTime PoSh for Windows Red Team"
+$Form.text  = "ClobberTime PowerShell for Windows Red Team"
 $Form.TopMost   = $True
 $Form.FormBorderStyle = 'Fixed3D'
 
@@ -98,11 +97,23 @@ $TimestompButton.height = 52
 $TimestompButton.location   = New-Object System.Drawing.Point(469,116)
 $TimestompButton.Font   = 'Global Sans Serif,17'
 
-$Label1 = New-Object system.Windows.Forms.Label
-$Label1.width   = 150
-$Label1.height  = 20
-$Label1.location  = New-Object System.Drawing.Point(220,94)
-$Label1.Font    = 'Microsoft Sans Serif,10'
+$TimeStampLabel1 = New-Object system.Windows.Forms.Label
+$TimeStampLabel1.width   = 150
+$TimeStampLabel1.height  = 20
+$TimeStampLabel1.location  = New-Object System.Drawing.Point(220,94)
+$TimeStampLabel1.Font    = 'Microsoft Sans Serif,10'
+
+$TimeStampLabel2 = New-Object system.Windows.Forms.Label
+$TimeStampLabel2.width   = 150
+$TimeStampLabel2.height  = 20
+$TimeStampLabel2.location  = New-Object System.Drawing.Point(220,118)
+$TimeStampLabel2.Font    = 'Microsoft Sans Serif,10'
+
+$TimeStampLabel3 = New-Object system.Windows.Forms.Label
+$TimeStampLabel3.width   = 150
+$TimeStampLabel3.height  = 20
+$TimeStampLabel3.location  = New-Object System.Drawing.Point(220,140)
+$TimeStampLabel3.Font    = 'Microsoft Sans Serif,10'
 
 $ModifiedTimestampCheckbox = New-Object system.Windows.Forms.Checkbox
 $ModifiedTimestampCheckbox.location  = New-Object System.Drawing.Point(390,90)
@@ -115,18 +126,6 @@ $AccessedTimestampCheckbox.Text = "Accessed"
 $CreatedTimestampCheckbox  = New-Object system.Windows.Forms.Checkbox
 $CreatedTimestampCheckbox.location  = New-Object System.Drawing.Point(390,150)
 $CreatedTimestampCheckbox.Text = "Created"
-
-$Label2 = New-Object system.Windows.Forms.Label
-$Label2.width   = 150
-$Label2.height  = 20
-$Label2.location  = New-Object System.Drawing.Point(220,118)
-$Label2.Font    = 'Microsoft Sans Serif,10'
-
-$Label3 = New-Object system.Windows.Forms.Label
-$Label3.width   = 150
-$Label3.height  = 20
-$Label3.location  = New-Object System.Drawing.Point(220,140)
-$Label3.Font    = 'Microsoft Sans Serif,10'
 
 $global:TargetDirectory;
 $global:SingularDate;
@@ -152,17 +151,17 @@ $BrowseButton.Add_Click(
 
 $RandomDateRadioButton.Add_Click(
 {
-    $Label1,$Label2,$Label3 | % {$_.Text = ""}
-    $NewDate = New-RandomDate;
-    $Label1.text = $NewDate;
+    $TimeStampLabel1,$TimeStampLabel2,$TimeStampLabel3 | % {$_.Text = ""}
+    $NewDate = New-RandomDate 
+    $TimeStampLabel1.text = $NewDate;
     $global:SingularDate = $NewDate;
 })
 
 $RandomDatesRadioButton.Add_Click(
 {
-    $Label1.text = $(New-RandomDate)
-    $Label2.text = $(New-RandomDate)
-    $Label3.text = "..."
+    $TimeStampLabel1.text = $(New-RandomDate)
+    $TimeStampLabel2.text = $(New-RandomDate)
+    $TimeStampLabel3.text = "..."
 })
 
 $DateSelectionRadioButton.Add_Click(
@@ -176,7 +175,7 @@ $DateSelectionRadioButton.Add_Click(
     $TimeInputTextBox.Font  = 'Microsoft Sans Serif,14'
     $TimeInputTextBox.MaxLength = 8;
 
-    $Label1,$Label2,$Label3 | % {$_.Text = ""}
+    $TimeStampLabel1,$TimeStampLabel2,$TimeStampLabel3 | % {$_.Text = ""}
     $CalendarForm = New-Object Windows.Forms.Form -Property @{
         StartPosition = [Windows.Forms.FormStartPosition]::CenterScreen
         Size          = New-Object Drawing.Size 320, 280
@@ -213,21 +212,21 @@ $DateSelectionRadioButton.Add_Click(
 
     if ($result -eq [Windows.Forms.DialogResult]::OK) 
     { 
-        $Date = $NewCalendar.SelectionStart
-
-        $NormalizedDate = Get-Date -Year $Date.Year -Month $Date.Month -Day $Date.Day `
+        if ($NewCalendar.SelectionRange.End.TimeOfDay.ToString() -match '^([0-1]?\d|2[0-3])(?::([0-5]?\d))?(?::([0-5]?\d))?$')
+        {    
+            $Date = $NewCalendar.SelectionStart
+            $NormalizedDate = Get-Date -Year $Date.Year -Month $Date.Month -Day $Date.Day `
             -Hour  $([regex]::Match($TimeInputTextBox.Text,'(?:([01]\d|2[0-3])):((?:[0-5]\d)):((?:[0-5]\d))').Groups[1].Value)`
             -Minute $([regex]::Match($TimeInputTextBox.Text,'(?:([01]\d|2[0-3])):((?:[0-5]\d)):((?:[0-5]\d))').Groups[2].Value) `
             -Second $([regex]::Match($TimeInputTextBox.Text,'(?:([01]\d|2[0-3])):((?:[0-5]\d)):((?:[0-5]\d))').Groups[3].Value)
-
-        $Label1.text = $NormalizedDate;
-        $global:SingularDate = $NormalizedDate
+            $TimeStampLabel1.text = $NormalizedDate;
+            $global:SingularDate = $NormalizedDate
+       }
     }
 })
 
 $TimestompButton.Add_Click(
 { 
-      
     if($FileBrowser.FileNames.Count -lt 1)
     {
         [System.Windows.MessageBox]::Show('First specify a set of targets to timestomp (click Browse...)')
@@ -235,42 +234,147 @@ $TimestompButton.Add_Click(
             
     else
     {
-        $FileBrowser.FileNames | % {   
-            if(![System.IO.File]::Exists($_)) 
-            {
-                [System.Windows.MessageBox]::Show('One or more target items are not valid paths.')
-            }
+        if(!$ModifiedTimestampCheckbox.Checked -and !$AccessedTimestampCheckbox.Checked -and !$CreatedTimestampCheckbox.Checked)
+        {
+            [System.Windows.MessageBox]::Show('Select one or more MAC properties to manipulate on the target file(s).')
+        } 
+        else 
+        {
+            $FileBrowser.FileNames | % {   
+                if(![System.IO.File]::Exists($_)) 
+                {
+                    [System.Windows.MessageBox]::Show('One or more target items are not valid paths.')
+                    break;
+                }
+                else
+                {
+                    if($RandomDateRadioButton.Checked ) 
+                    {
+                            $FileBrowser.FileNames  | % {
+                            if($ModifiedTimestampCheckbox.Checked){Timestomp-Files -Modified -Path $_ -TargetDate $global:SingularDate}
+                            if($AccessedTimestampCheckbox.Checked){Timestomp-Files -Accessed -Path $_ -TargetDate $global:SingularDate}
+                            if($CreatedTimestampCheckbox.Checked){Timestomp-Files -Created -Path $_ -TargetDate $global:SingularDate}
+                                                        }
+                    }
+                    if($RandomDatesRadioButton.Checked ) 
+                    {
+                            $FileBrowser.FileNames  | % {
+                            if($ModifiedTimestampCheckbox.Checked){Timestomp-Files -Modified -Path $_}
+                            if($AccessedTimestampCheckbox.Checked){Timestomp-Files -Accessed -Path $_}
+                            if($CreatedTimestampCheckbox.Checked){Timestomp-Files -Created -Path $_}
+                                                        }
+                    }
+                    if($DateSelectionRadioButton.Checked ) 
+                    {
+                            $FileBrowser.FileNames  | % {
+                            if($ModifiedTimestampCheckbox.Checked){Timestomp-Files -Modified -Path $_ -TargetDate $global:SingularDate}
+                            if($AccessedTimestampCheckbox.Checked){Timestomp-Files -Accessed -Path $_ -TargetDate $global:SingularDate}
+                            if($CreatedTimestampCheckbox.Checked){Timestomp-Files -Created -Path $_ -TargetDate $global:SingularDate}
+                                                        }
+                    }
 
-        if($RandomDateRadioButton.Checked ) 
-        {
-            $FileBrowser.FileNames  | % {
-                if($ModifiedTimestampCheckbox.Checked){Timestomp-Files -Modified -Path $_ -TargetDate $global:SingularDate}
-                if($AccessedTimestampCheckbox.Checked){Timestomp-Files -Accessed -Path $_ -TargetDate $global:SingularDate}
-                if($CreatedTimestampCheckbox.Checked){Timestomp-Files -Created -Path $_ -TargetDate $global:SingularDate}
-        }
-        }
-        if($RandomDatesRadioButton.Checked ) 
-        {
-            $FileBrowser.FileNames  | % {
-            
-                if($ModifiedTimestampCheckbox.Checked){Timestomp-Files -Modified -Path $_}
-                if($AccessedTimestampCheckbox.Checked){Timestomp-Files -Accessed -Path $_}
-                if($CreatedTimestampCheckbox.Checked){Timestomp-Files -Created -Path $_}
-                                        }
-        }
-        if($DateSelectionRadioButton.Checked ) 
-        {
-            $FileBrowser.FileNames  | % {
-                if($ModifiedTimestampCheckbox.Checked){Timestomp-Files -Modified -Path $_ -TargetDate $global:SingularDate}
-                if($AccessedTimestampCheckbox.Checked){Timestomp-Files -Accessed -Path $_ -TargetDate $global:SingularDate}
-                if($CreatedTimestampCheckbox.Checked){Timestomp-Files -Created -Path $_ -TargetDate $global:SingularDate}
-                                        }
-        }
+                }
                                    }
+    }
     }
 })
 
-$Form.controls.AddRange(@($DateTypeGroupBox,$PathDescriptorTextBox,$BrowseButton,$TimestompButton,$Label1,$Label2,$Label3,$ModifiedTimestampCheckbox,$AccessedTimestampCheckbox,$CreatedTimestampCheckbox))
+$Form.controls.AddRange(@($DateTypeGroupBox,$PathDescriptorTextBox,$BrowseButton,$TimestompButton,$TimeStampLabel1,$TimeStampLabel2,$TimeStampLabel3,$ModifiedTimestampCheckbox,$AccessedTimestampCheckbox,$CreatedTimestampCheckbox))
 $DateTypeGroupBox.controls.AddRange(@($RandomDateRadioButton,$RandomDatesRadioButton,$DateSelectionRadioButton))
+
+<# 
+.SYNOPSIS 
+    Return a .NET DateTime object that falls within a specified range.
+    Vasken Houdoverdov
+.DESCRIPTION 
+    Part of a module that facilitiates the generation of random DateTime objects and the modification of FileInfo objects to the resulting dates.
+.EXAMPLE
+    New-RandomDate
+#> 
+
+Function New-RandomDate {
+$Ceiling = Get-Date -Year 2020 -Month 12 -Date 31
+$Floor = Get-Date -Year 2000 -Month 12 -Date 31
+Return $(New-Object DateTime($(Get-Random -Max $Ceiling.Ticks -Min $Floor.Ticks))).ToUniversalTime()
+}
+
+<# 
+.SYNOPSIS 
+    Utility for timestomping files in the specified path.  
+    Several properties of object type System.IO.FileInfo are modified as a result.
+    Vasken Houdoverdov
+.DESCRIPTION 
+    Part of a module that facilitiates the generation of random DateTime objects and the modification of FileInfo objects to the resulting dates.
+.Example 
+    Timestomp-Files $Path
+#> 
+
+Function Timestomp-Files {
+Param([string]$Path,
+      [Datetime]$TargetDate,
+      [Switch]$Modified,
+      [Switch]$Accessed,
+      [Switch]$Created
+     )
+
+if($TargetDate -eq $NULL) {
+
+if ($Path -isnot [System.IO.DirectoryInfo]){  
+if($Modified)
+{
+$(Get-Item $Path).LastWriteTime = $(New-RandomDate)
+$(Get-Item $Path).LastWriteTimeUTC = $(New-RandomDate)
+}
+
+if($Accessed)
+{
+$(Get-Item $Path).LastAccessTime = $(New-RandomDate)
+$(Get-Item $Path).LastAccessTimeUTC = $(New-RandomDate)
+}
+
+if($Created)
+{
+$(Get-Item $Path).CreationTime = $(New-RandomDate)
+$(Get-Item $Path).CreationTimeUTC = $(New-RandomDate)}
+}
+
+else
+{  
+if(!$(Test-Path $Path)){throw "The specified path wasn't found:  $Path"}
+if(!$(Test-Path "$Path\*")){throw "The specified path was empty:  $Path"}
+}
+}
+
+else
+{
+    if ($Path -isnot [System.IO.DirectoryInfo])
+    {  
+        if($Modified)
+        {
+            $(Get-Item $Path).LastWriteTime = $TargetDate
+            $(Get-Item $Path).LastWriteTimeUTC = $TargetDate
+        }
+
+    if($Accessed)
+        {
+            $(Get-Item $Path).LastAccessTime = $TargetDate
+            $(Get-Item $Path).LastAccessTimeUTC = $TargetDate
+        }
+
+    if($Created)
+        {
+            $(Get-Item $Path).CreationTime = $TargetDate
+            $(Get-Item $Path).CreationTimeUTC = $TargetDate
+        }
+}
+
+    else
+    {
+        if(!$(Test-Path $Path)){throw "The specified path wasn't found:  $Path"}
+        if(!$(Test-Path "$Path\*")){throw "The specified path was empty:  $Path"}
+
+    }
+}
+}
 
 [void]$Form.ShowDialog()
