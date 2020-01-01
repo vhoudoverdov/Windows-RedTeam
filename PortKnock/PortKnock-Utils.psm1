e783c3102f1e22d80a49a0091221fcafe363724b
Vasken Houdoverdov <vhcomptech@gmail.com>
2019-12-31 23:07:06 -0800

    PortKnock Utils

---
+++ PortKnock/PortKnock-Utils.psm1
@@ -0,0 +1,101 @@
+﻿<# 
+.SYNOPSIS 
+    Provides an interface for port-knocking over TCP.  Designed for use during Windows Red Team and Pentesting engagements.
+    This tool was designed specifically to provide a PowerShell-based port knocking utility that allows source ports to be specified as part of the port-knock operation.
+    It was also designed as an educational tool for understanding advanced persistance mechanisms used by threat actors and malware.
+
+    Why is specifying a source port significant in port-knocking?
+        A number of Linux rootkits and associated persistence mechanisms (userland backdoors) have been observed using logic that considers source ports from the client side.
+        Examples of these rootkits include Venom [1][2] and similar Linux-based rootkits used during the 2014 Freenode compromise [3].
+        This tool was designed to implement similar functionality using PowerShell and .NET, while also being an educational reference to this class of malware.
+    
+        Reference and further reading:
+        [1] https://security.web.cern.ch/security/advisories/venom/venom.shtml
+        [2] https://www.it.ucla.edu/security/advisories/new-linux-kernel-venom-rootkit-in-the-wild 
+        [3] https://www.nccgroup.trust/uk/about-us/newsroom-and-events/blogs/2014/october/analysis-of-the-linux-backdoor-used-in-freenode-irc-network-compromise/
+
+.AUTHOR
+    Vasken Houdoverdov
+    github.com/vhoudoverdov
+
+.DESCRIPTION 
+    Provides an interface for port-knocking over TCP.  Designed for use during Windows Red Team and Pentesting engagements.
+    This tool is also designed as an educational tool for understanding advanced persistance mechanisms used by threat actors and malware.
+
+.EXAMPLE
+    New-TcpPortKnock -LocalIP "10.32.0.2" -LocalPort 1337 -RemoteIp "151.101.1.140" -RemotePort 443
+#> 
+
+Function New-TcpPortKnock()
+{
+    param (
+        [String]$LocalIP, 
+        [int]$LocalPort,
+        [String]$RemoteIp,    
+        [int]$RemotePort
+          )
+
+    try {
+        $LocalIPEndPoint  = New-Object Net.IPEndPoint ([IPAddress]::Parse($LocalIP),$LocalPort)
+        $RemoteIPEndPoint  = New-Object Net.IPEndPoint ([IPAddress]::Parse($RemoteIp),$RemotePort)
+        $TcpClient = New-Object Net.Sockets.TcpClient($LocalIPEndPoint)
+        $TcpClient.connect($RemoteIPEndPoint)
+        write-host “Connected.”
+        $TcpClient.close()
+        }
+    catch {
+        write-host “Error connecting.”
+          }
+}
+
+<# 
+.SYNOPSIS 
+    Provides an interface for port-knocking over UDP.  Designed for use during Windows Red Team and Pentesting engagements.
+    This tool was designed specifically to provide a PowerShell-based port knocking utility that allows source ports to be specified as part of the port-knock operation.
+    It was also designed as an educational tool for understanding advanced persistance mechanisms used by threat actors and malware.
+    
+    Why is specifying a source port significant in port-knocking?
+        A number of Linux rootkits and associated persistence mechanisms (userland backdoors) have been observed using logic that considers source ports from the client side.
+        Examples of these rootkits include Venom [1][2] and similar Linux-based rootkits used during the 2014 Freenode compromise [3].
+        This tool was designed to implement similar functionality using PowerShell and .NET, while also being an educational reference to this class of malware.
+    
+        Reference and further reading:
+        [1] https://security.web.cern.ch/security/advisories/venom/venom.shtml
+        [2] https://www.it.ucla.edu/security/advisories/new-linux-kernel-venom-rootkit-in-the-wild 
+        [3] https://www.nccgroup.trust/uk/about-us/newsroom-and-events/blogs/2014/october/analysis-of-the-linux-backdoor-used-in-freenode-irc-network-compromise/
+
+.AUTHOR
+    Vasken Houdoverdov
+    github.com/vhoudoverdov
+
+.DESCRIPTION 
+    Provides an interface for port-knocking over UDP.  Designed for use during Windows Red Team and Pentesting engagements.
+    This tool is also designed as an educational tool for understanding advanced persistance mechanisms used by threat actors and malware.
+
+.EXAMPLE
+    New-UdpPortKnock -LocalIP "10.32.0.2" -LocalPort 1337 -RemoteIp "8.8.8.8" -RemotePort 53
+#> 
+
+Function New-UdpPortKnock()
+{
+    param (
+        [String]$LocalIP, 
+        [int]$LocalPort,
+        [String]$RemoteIp,    
+        [int]$RemotePort
+          )
+
+    try {
+        $LocalIPEndPoint  = New-Object Net.IPEndPoint ([IPAddress]::Parse($LocalIP),$LocalPort)
+        $RemoteIPEndPoint  = New-Object Net.IPEndPoint ([IPAddress]::Parse($RemoteIp),$RemotePort)
+        $UdpClient = new-Object Net.Sockets.Udpclient($LocalPort)
+        $UdpClient.connect($RemoteIPEndPoint)  
+       # $bytes = [System.Text.Encoding]::Unicode.GetBytes("text")
+       # $udpobject.Send($bytes, $bytes.length())
+        write-host “Connected.”
+       $UdpClient.close()
+        }
+    catch {
+        write-host “Error connecting.”
+          }
+}
---
+++ PortKnock/README.md
@@ -0,0 +1,61 @@
+# PortKnock-Utils
+
+## Table of Contents
+* [Intro](#intro)
+* [Supported Operations](#operations)
+* [Examples](#examples)
+* [Malware Use Cases and Further Reading](#appendix)
+
+### <a name="intro"></a>Intro
+PortKnock-Utils is a PowerShell interface for performing port-knock operations during Windows Red Team and Pentesting engagements.
+This tool was designed specifically to provide a port knocking utility that allows source ports to be specified as part of the port-knock operation.
+It was also designed as an educational tool for understanding advanced persistance mechanisms used by threat actors and malware.
+
+#####  Author
+Vasken Houdoverdov  - github.com/vhoudoverdov
+
+### <a name="operations"></a>Supported Operations
+
+| Operation | Description |
+| --- | --- |
+| **Invoke a TCP port knock** | Construct and initiate one or more port knocks over TCP. |
+| **Invoke a UDP port knock** | Construct and initiate one or more port knocks over UDP.|
+
+### <a name="examples"></a>Examples
+###### Construct a TCP Port Knock Sequence
+```
+# C2 Server is waiting for a sequence of three TCP knocks whose source ports are prime numbers...
+
+$CommandAndControl = "10.66.0.1"
+
+New-TcpPortKnock -LocalIP "10.32.0.2" -LocalPort 2137 -RemoteIp $CommandAndControl -RemotePort 8080
+New-TcpPortKnock -LocalIP "10.32.0.2" -LocalPort 4099 -RemoteIp $CommandAndControl -RemotePort 8080
+New-TcpPortKnock -LocalIP "10.32.0.2" -LocalPort 3761 -RemoteIp $CommandAndControl -RemotePort 8080
+New-TcpPortKnock -LocalIP "10.32.0.2" -LocalPort 1999 -RemoteIp $CommandAndControl -RemotePort 8080
+```
+
+
+###### Construct a UDP Port Knock Sequence
+```
+# C2 Server is waiting for a sequence of four UDP knocks whose source ports are identical, and where there is a two second delay between each knock...
+
+$CommandAndControl = "10.66.0.1"
+
+1..4 | % {
+  New-TcpPortKnock -LocalIP "10.32.0.2" -LocalPort 1337 -RemoteIp $CommandAndControl -RemotePort 8080
+  Sleep 2;
+}
+```
+### <a name="appendix"></a>Malware Use Cases and Further Reading
+A number of interesting malwares have utilized port knocking in combination with source port interpretation.  Two examples of this type of malware are provided here.
+
+#### Venom Rootkit (2017)
+
+[CERN - Advisory: VENOM Linux rootkit](https://security.web.cern.ch/security/venom.shtml)
+
+[UCLA - New Linux Kernel "VENOM" Rootkit in the Wild](https://www.it.ucla.edu/security/advisories/new-linux-kernel-venom-rootkit-in-the-wild)
+
+
+#### Linux Malware from Freenode Intrusion (2014)
+
+[Analysis of the Linux backdoor used in freenode IRC network intrusion](https://www.nccgroup.trust/uk/about-us/newsroom-and-events/blogs/2014/october/analysis-of-the-linux-backdoor-used-in-freenode-irc-network-compromise/)
